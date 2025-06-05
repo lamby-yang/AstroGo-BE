a28adbcaa9de0434442347919db67eb39f4f56c5
lamby-yang <18513512808@163.com>
2025-06-06 00:49:38 +0800

    修改message的一点点bug

--- message/urls.py
+++ message/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('login_user/', views.login_user),
     path('register_user/', views.register_user),
-    path('user_info/<int:item_id>/', views.user_info),
-    path('exercise_reminders/<int:item_id>/', views.exercise_reminders),
+    path('user_info/<int:uid>/', views.user_info),
+    path('exercise_reminders/<int:uid>/', views.exercise_reminders),
     path('check_phone/<str:phone_number>/', views.check_phone),
     path('exercise_overview/<int:uid>/', views.exercise_overview),
 ]
